646a663141cb88e3a361fc30b4cbb1a082fb811d
foxbloodiron <ariakbar6944@gmail.com>
2019-03-26 16:45:43 +0700

    hapus breadcrumb & list pelaksanaan manasik otw

---
+++ www/includes/manasik/pelaksanaan_manasik/tab_pelaksanaan_manasik/js/index-tab-pelaksanaan-commander.js
@@ -0,0 +1,67 @@
+$(document).ready(function(){
+	$.ajax({
+	      url: url('api/marketing/manasik/pelaksanaan_kegiatan/list_pelaksanaan'),
+	      type    : 'get',
+	      dataType : 'json',
+	      success : function(resp){
+	      	var card = $('.card');
+	        units = resp;
+	        var unit, rawcontent, content;
+	        console.log(units);
+	        // if(units.length > 0) {
+	        // 	rawcontent = $('rawcontent.datalist').html();
+	        // 	for(x = 0;x < units.length;x++) {
+	        // 		unit = units[x];
+	        // 		console.log(unit);
+	        // 		content = $(rawcontent);
+	        // 		content.append('<input type="hidden" name="id_perencanaan" id="id_perencanaan" value="'+ unit.mp_id +'">')
+	        // 		// Set-up routing untuk menampilkan preview
+	        // 		content.attr('pusatriyal-role', 'routing');
+	        // 		content.attr('pusatriyal-target', 'includes/manasik/perencanaan_manasik/preview-perencanaan.html');
+	        // 		content.attr('pusatriyal-callback', 'preview_manasik_plan');
+	        // 		content.append(
+	        // 			'<input type="hidden" name="mp_id" value="' + unit.mp_id + '">'
+	        // 		)
+	        // 		content.find('.card-title').text( 
+	        // 			unit.mp_code
+	        // 		);
+	        // 		content.find('.card-subtitle').text( 
+	        // 			unit.mp_place
+	        // 		);
+	        // 		content.find('.card-info-first').text( 
+	        // 			$(unit.tgl).text()
+	        // 		);
+	        // 		content.find('.card-info-second').text( 
+	        // 			$(unit.status).text()
+	        // 		);
+
+	        // 		$('.card').append(content);
+	        // 	}
+
+	        // 	Routing.load_routing();
+	        // }
+	        // else {
+	        // 	card.html('<div class="card-body"><h4 class="card-title">Tidak Ada Data Yang Ditampilkan</h4></div>')
+	        // }
+	      },
+	      error: function(jqXHR, exception) {
+	        if (jqXHR.status === 0) {
+	            alert('Not connect.\n Verify Network.');
+	        } else if (jqXHR.status == 404) {
+	            alert('Requested page not found. [404]');
+	        } else if (jqXHR.status == 500) {
+	            alert('Internal Server Error [500].');
+	        } else if (exception === 'parsererror') {
+	            alert('Requested JSON parse failed.');
+	        } else if (exception === 'timeout') {
+	            alert('Time out error.');
+	        } else if (exception === 'abort') {
+	            alert('Ajax request aborted.');
+	        } else {
+	            alert('Uncaught Error.\n' + jqXHR.responseText);
+	        }
+	      }
+    });
+
+    
+});
