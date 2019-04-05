e8a9f6a2ac947187648e67d14455cefec8b6b7e4
foxbloodiron <ariakbar6944@gmail.com>
2019-04-05 14:03:37 +0700

    select m_comp fixed

--- www/config/config.js
+++ www/config/config.js
@@ -1,5 +1,5 @@
-host = 'http://127.0.0.1/newpusatriyal/';
-client_secret = 'HLyFA0xhYBtB1d7ULUX4Ta0guEnrPqLKr5mhSIat';
+host = 'http://192.168.100.21/git/pusatriyal/';
+client_secret = 'ara8OfuKd2Z3uTdQh3gj5RyrLAqB7JnwfNlcCisn';
 client_id = 2;
 grant_type = 'password';
 
