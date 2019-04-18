1c0bc371c9d9e660701e2094095392a56213a926
FvorY <ferdyp73@gmail.com>
2019-04-18 13:53:16 +0700

    .

--- www/config/config.js
+++ www/config/config.js
@@ -9,13 +9,3 @@ url = function(pathname) {
 
     return outp;
 }
-// Initialize Firebase
-var config = {
-	apiKey: "AIzaSyBBP3aTEZ61LMFe933ijJUEz_aWsdorJks",
-	authDomain: "alamrayasite-pusatriyal.firebaseapp.com",
-	databaseURL: "https://alamrayasite-pusatriyal.firebaseio.com",
-	projectId: "alamrayasite-pusatriyal",
-	storageBucket: "alamrayasite-pusatriyal.appspot.com",
-	messagingSenderId: "458093057695"
-};
-firebase.initializeApp(config);
