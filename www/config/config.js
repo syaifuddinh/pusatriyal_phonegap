c8995d68769b12112aeffe5f70cbc1005d2172c4
foxbloodiron <ariakbar6944@gmail.com>
2019-05-10 14:25:40 +0700

    form pelaksanaan kegiatan manasik done

--- www/config/config.js
+++ www/config/config.js
@@ -1,4 +1,4 @@
-host = 'http://192.168.100.11/git/pusatriyal/';
+host = 'http://192.168.100.19/git/pusatriyal/';
 client_secret = 'HLyFA0xhYBtB1d7ULUX4Ta0guEnrPqLKr5mhSIat';
 client_id = 2;
 grant_type = 'password';
