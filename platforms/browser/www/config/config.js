7f57a04fc5aad713e47384c22ae2d2ce6447bc83
FvorY <ferdyp73@gmail.com>
2019-05-30 16:14:52 +0700

    .

--- platforms/browser/www/config/config.js
+++ platforms/browser/www/config/config.js
@@ -1,11 +1,11 @@
-host = 'http://localhost/pusatriyal/';
+host = 'http://127.0.0.1/newpusatriyal/';
 client_secret = 'HLyFA0xhYBtB1d7ULUX4Ta0guEnrPqLKr5mhSIat';
 client_id = 2;
 grant_type = 'password';
 
 url = function(pathname) {
-    var pathname = pathname.replace(/$\/(.*)/);
-    var outp = host + pathname;
+	var pathname = pathname.replace(/$\/(.*)/);
+	var outp = host + pathname;
 
-    return outp;
+	return outp;
 }
